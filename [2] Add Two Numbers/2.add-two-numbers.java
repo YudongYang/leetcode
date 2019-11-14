de7a2c3df2609cdc95c321cb4d89ae598e31ddb2
don.yang <don.yang@rccchina.com>
2019-11-14 12:59:08 +0800

    2.add-two-numbers

--- [2] Add Two Numbers/2.add-two-numbers.java
+++ [2] Add Two Numbers/2.add-two-numbers.java
@@ -17,21 +17,28 @@ class Solution {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     ListNode l3 = null;
     ListNode result = null;
-    int a, b, x = 0, y;
+    int a, b, x = 0, y, r;
     while (l1 != null || l2 != null) {
-      a = l1 != null ? l1.val : 0;
-      b = l2 != null ? l2.val : 0;
-      y = (x + a + b) % 10;
-      if (l3 == null) {
-        l3 = new ListNode(y);
-        result = l3;
+      if (l1 != null) {
+        a = l1.val; l1 = l1.next;
       } else {
+        a = 0; l1 = null;
+      }
+      if (l2 != null) {
+        b = l2.val; l2 = l2.next;
+      } else {
+        b = 0; l2 = null;
+      }
+      r = x + a + b;
+      y = r % 10;
+      if (l3 != null) {
         l3.next = new ListNode(y);
         l3 = l3.next;
+      } else {
+        l3 = new ListNode(y);
+        result = l3;
       }
-      x = (x + a + b) / 10;
-      l1 = l1 != null ? l1.next : null;
-      l2 = l2 != null ? l2.next : null;
+      x = r / 10;
     }
     if (x != 0) {
       l3.next = new ListNode(x);
