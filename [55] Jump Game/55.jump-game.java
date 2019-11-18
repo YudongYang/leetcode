90dee89c71532e22c589cda2db610e82d129dde1
don.yang <don.yang@rccchina.com>
2019-11-18 16:36:34 +0800

    55.jump-game

---
+++ [55] Jump Game/55.jump-game.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=55 lang=java
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+class Solution {
+  public boolean canJump(int[] nums) {
+    int[] r = new int[nums.length];
+    r[0] = 1;
+    for (int i = 0; i < (nums.length - 1); i++) {
+      if (r[i] != 1) {
+        continue;
+      }
+      int n = nums[i];
+      for (int j = 1; j <= n && (i + j) < nums.length; j++) {
+        r[(i + j)] = 1;
+      }
+    }
+    return r[nums.length - 1] == 1;
+  }
+}
+// @lc code=end
+
