4e0f5928b09161c321a04cb463050ff6684e068e
don.yang <don.yang@rccchina.com>
2019-11-18 16:55:01 +0800

    55.jump-game

--- [55] Jump Game/55.jump-game.java
+++ [55] Jump Game/55.jump-game.java
@@ -7,18 +7,25 @@
 // @lc code=start
 class Solution {
   public boolean canJump(int[] nums) {
-    int[] r = new int[nums.length];
-    r[0] = 1;
-    for (int i = 0; i < (nums.length - 1); i++) {
-      if (r[i] != 1) {
-        continue;
-      }
-      int n = nums[i];
-      for (int j = 1; j <= n && (i + j) < nums.length; j++) {
-        r[(i + j)] = 1;
+    // int[] r = new int[nums.length];
+    // r[0] = 1;
+    // for (int i = 0; i < (nums.length - 1); i++) {
+    //   if (r[i] != 1) {
+    //     continue;
+    //   }
+    //   int n = nums[i];
+    //   for (int j = 1; j <= n && (i + j) < nums.length; j++) {
+    //     r[(i + j)] = 1;
+    //   }
+    // }
+    // return r[nums.length - 1] == 1;
+    int p = nums.length - 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      if ((i + nums[i]) >= p) {
+        p = i;
       }
     }
-    return r[nums.length - 1] == 1;
+    return p == 0;
   }
 }
 // @lc code=end
